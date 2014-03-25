5066e3dae989478a1ba7bea57851053b60fd2d50
Vadim Filanovsky <vfilanovsky@netflix.com>
2014-03-25 16:34:51 -0700

    for accounts that were not mapped to names in ice.properties (ice.account.xxx), this check will make sure that data/tags are updated properly once the mapping is established in ice.properties

--- src/java/com/netflix/ice/basic/BasicAccountService.java
+++ src/java/com/netflix/ice/basic/BasicAccountService.java
@@ -60,6 +60,11 @@ public class BasicAccountService implements AccountService {
 
     public Account getAccountByName(String accountName) {
         Account account = accountsByName.get(accountName);
+        // for accounts that were not mapped to names in ice.properties (ice.account.xxx), this check will make sure that
+        // data/tags are updated properly once the mapping is established in ice.properties
+        if (account == null) {
+            account = accountsById.get(accountName);
+        }
         if (account == null) {
             account = new Account(accountName, accountName);
             accountsByName.put(account.name, account);
