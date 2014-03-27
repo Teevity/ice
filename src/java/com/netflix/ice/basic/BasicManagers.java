6f69154e69d204b96eb290fa965709ec77cdcf99
Anthony Johnson <anthony.johnson@nokia.com>
2014-03-27 14:12:06 -0400

    Support for Services with a '/' in them

--- src/java/com/netflix/ice/basic/BasicManagers.java
+++ src/java/com/netflix/ice/basic/BasicManagers.java
@@ -99,6 +99,7 @@ public class BasicManagers extends Poller implements Managers {
             }
             else {
                 String name = key.substring((config.workS3BucketPrefix + TagGroupWriter.DB_PREFIX).length());
+                name = name.replaceAll("--", "/");
                 product = config.productService.getProductByName(name);
             }
             if (!products.contains(product)) {
